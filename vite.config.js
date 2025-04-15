27aa0f8f4a09c2c17acb037a6204db45c8c45959
MithunGowda <mithungowdahc12a@gmail.com>
2025-04-15 22:40:55 +0530

    Google tag added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Rishabh-CI-CD/", // Change to your GitHub repo name
+  base: "/", // Change to your GitHub repo name
   plugins: [react()],
 });
