aa0560369edd18c4a6fb60fd6d9602fbad47baf0
MithunGowda <mithungowdahc12a@gmail.com>
2025-09-10 17:10:36 +0530

    new container1

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -77,34 +77,27 @@ const Product = () => {
               ))}
             </div>
           </div>
-          <button
-            onClick={() => {
-              if (!size) {
-                alert("Please select a size before adding to cart");
-                return;
-              }
-              window.dataLayer = window.dataLayer || [];
-  window.dataLayer.push({
-    event: "add_to_cart",
-    ecommerce: {
-      items: [
-        {
-          item_id: productData._id,
-          item_name: productData.name,
-          price: productData.price,
-          currency: currency,
-          item_variant: size
-        }
-      ]
+         <button
+  onClick={() => {
+    if (!size) {
+      alert("Please select a size before adding to cart");
+      return;
     }
-  });
-              addToCart(productData._id, size);
-              navigate("/cart");
-            }}
-            className=" bg-black text-white px-8 py-3 text-sm active:bg-gray-700"
-          >
-            ADD TO CART
-          </button>
+
+    // Simple event push
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: "add_to_cart"
+    });
+
+    addToCart(productData._id, size);
+    navigate("/cart");
+  }}
+  className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700"
+>
+  ADD TO CART
+</button>
+
 
           <hr className=" mt-8 sm:w-4/5" />
           <div className=" text-sm text-gray-500 mt-5 flex  flex-col gap-1">
